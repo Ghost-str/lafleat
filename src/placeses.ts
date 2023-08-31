ebaea6d29315db3c477f46a832ef0402f11071dd
ghost-str <Ghost-str@users.noreply.github.com>
2023-08-31 20:45:44 +0700

    linting

--- src/placeses.ts
+++ src/placeses.ts
@@ -1,5 +1,3 @@
-
-
 const data = `Саратов	г. Саратов, пл. Орджоникидзе, 1	51,501224	45,944021
 Саратов	г. Саратов, ул. Танкистов, 1	51,546939	46,014673
 Саратов	г. Саратов, ул.Гвардейская, 2А	51,585463	45,946087
@@ -8,19 +6,14 @@ const data = `Саратов	г. Саратов, пл. Орджоникидзе,
 Саратов	г. Саратов, ул. Симбирская, д. 154	51,556577	46,045638
 Саратов	г. Саратов, Вольский тракт, 2А	51,618636	45,971222`;
 
-let places = data
-    .split("\n")
-    .map(
-        (row) =>  {
-            let columns= row.split("	"); 
-            return  {
-                city: columns[0],
-                addres: columns[1],
-                lat: +columns[2].replace(',','.'),
-                long: +columns[3].replace(',','.'),
-            }
-        }
-    );
-
+const places = data.split('\n').map((row) => {
+	const columns = row.split('	');
+	return {
+		city: columns[0],
+		addres: columns[1],
+		lat: Number(columns[2].replace(',', '.')),
+		long: Number(columns[3].replace(',', '.')),
+	};
+});
 
 export default places;
