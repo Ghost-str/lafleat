a56a2d39c9a2eb707a3d555268359f981c58391c
Dmitry <Ghost-str@users.noreply.github.com>
2023-08-31 19:45:47 +0700

    Fix web app loading (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import "./app-styles.css";
 import places from "./placeses";
 import marker_icon from "leaflet/dist/images/marker-icon.png"
+import telegramSdk from "./telegram-sdk";
 
 const marker_icon_inst = L.icon({
     iconUrl: marker_icon,
 
 });
 
+const map = L.map('map');
 
-
-
-const map = L.map('map').setView([51.501224, 45.944021], 18);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
@@ -22,4 +21,7 @@ places.forEach((place)=> {
    L.marker([place.lat,place.long], { icon: marker_icon_inst}).addTo(map)
 });
 
+map.setView([51.501224, 45.944021], 18)
 
+
+telegramSdk.ready();
